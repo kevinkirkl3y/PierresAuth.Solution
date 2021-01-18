4485b5ab3294f7be19ca7978bd6f2c760a930d5f
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-01-18 11:29:06 -0800

    Added viewbags to the splash page.

--- PierresAuth/Controllers/HomeController.cs
+++ PierresAuth/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using PierresAuth.Models;
 
 namespace PierresAuth.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresAuthContext _db;
+    public HomeController(PierresAuthContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
+      List<Treat> allTreats = _db.Treats.ToList();
+      List<Flavor> allFlavors = _db.Flavors.ToList();
+      ViewBag.AllTreats = allTreats;
+      ViewBag.allFlavors = allFlavors;
+      ViewBag.Treats = _db.Treats;
+      ViewBag.Flavors = _db.Flavors;
       return View();
     }
   }
